d5bdcdc23003cc85d8f4132485080ccbe7b2876c
Shak <ariknon@gmail.com>
2025-10-24 21:36:43 -0400

    Added square class

--- Square.java
+++ Square.java
@@ -1,10 +1,11 @@
+package B07lab5;
 
 public class Square {
 	double side_length;
 	
 	public Square(double x) 
 		side_length = x;
-	
+	//hello github -w- 
 	public double calc_area() 
 		return side_length * side_length;
 	
